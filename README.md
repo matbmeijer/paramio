54802e7a27e2b8ebe14d874234291552fb7fc137
matbmeijer <matthias@brennotten.net>
2022-11-01 21:49:48 +0100

    Added develop branch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Paramio
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 This package is in the making
--- paramio/saveformatting.py
+++ paramio/saveformatting.py
@@ -1,3 +1,7 @@
 class SaveFormatting(dict):
+    """
+    # If you try to access a key that doesn't exist, it will return the key surrounded by curly braces
+    """
+
     def __missing__(self, key):
         return "{" + key + "}"
