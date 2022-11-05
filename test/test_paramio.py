d5e39eb213035987024313ee3d17c5a27845434a
matbmeijer <matthias@brennotten.net>
2022-11-05 18:40:31 +0100

    Added tests to check classes are ignored

--- test/test_paramio.py
+++ test/test_paramio.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from paramio.paramio import update_config
 
 
@@ -5,6 +7,10 @@ def test_update_config_1():
     config_file = {
         "env": "{env}",
         "s3_bucket": "{bucket}",
+        "iterations": 1,
+        "{variable_key}": 10,
+        "numpy": np.array([1]),
+        "runs": ["{run}_{env}", "{run}_{env}", "{run}_{env}"],
         "group": {"task": "{bucket}/{group}/{task}/{experiment}"},
     }
 
@@ -19,6 +25,10 @@ def test_update_config_1():
     test_res = {
         "env": "dev",
         "s3_bucket": "enterprise_dwh_global",
+        "iterations": 1,
+        "{variable_key}": 10,
+        "numpy": np.array([1]),
+        "runs": ["{run}_dev", "{run}_dev", "{run}_dev"],
         "group": {"task": "enterprise_dwh_global/extract/read_origins/{experiment}"},
     }
 
